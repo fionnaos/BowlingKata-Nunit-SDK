46a8f9cb2110300e79367f294c964891cbfc5638
Fionna O'Sullivan <fionna.osullivan@ocuco.com>
2019-09-11 13:24:02 +0100

    Initial commit

---
+++ Bowling.Specflow/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("999efa28-8498-4b70-a80d-979fbd7b840e")]
